14f16a51cd5ca03e73677d216f08c9e4670b2817
djek9007 <djek9007@gmail.com>
2024-11-22 10:41:25 +0500

    media static

--- my_project/settings.py
+++ my_project/settings.py
@@ -11,7 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-s!c_a1cb96)3(m1_9(90qvd#jwhqf^8&bzlz^_quo-r')  # Устанавливаем значение по умолчанию
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') + ['0.0.0.0', '45.61.136.175']
+
 
 CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
 
