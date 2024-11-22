c68b399732ec3a353f4ab6415249eea004b108aa
djek9007 <djek9007@gmail.com>
2024-11-22 13:12:15 +0500

    media static

--- my_project/settings.py
+++ my_project/settings.py
@@ -14,6 +14,7 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') + ['0.0.0.0', '45.61.136.175']
 
 CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 
