0d8bac57195a0e975fd476de59a9196b6e93eda8
djek9007 <djek9007@gmail.com>
2024-11-22 10:51:13 +0500

    media static

--- my_project/settings.py
+++ my_project/settings.py
@@ -13,7 +13,6 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') + ['0.0.0.0', '45.61.136.175']
 
-
 CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
 
 
